40b41f2888aa9256e0a95e90ef5678e2ac76f97f
hannah <hggorel@smcm.edu>
2021-02-04 13:45:25 -0500

    Added header comment and setParams function

--- WumpusAgent.py
+++ WumpusAgent.py
@@ -1,1 +1,19 @@
+#Hannah Gorel, William Capon, Christian Oliverio, Liam Pratt
+#WumpusAgent.py
+#Date: 2/4/2021
+#Assignment:Project 1: Hunt the Wumpus
+#Description: Agent designed to retrieve the gold from the cave
+#while avoiding pits and the wumpus, and then escape alive!
+
+#parameters for type of game
+gametype=0
+numarrows=0
+numwumpi=0
+
+def setParams(type, arrows, wumpi):
+    gametype=type
+    numarrows = arrows
+    numwumpi = wumpi
+
+
 print("Hello")
