82dbcbbd49fb0d7075e1fae8fc128ab429cfdd19
Christian <cmoliverio.10@gmail.com>
2021-02-09 17:38:06 -0500

    Agent can now keep track of it's position, as well as the virtual origin! Agent also has a map that will grow as it explores.

--- WumpusAgent.py
+++ WumpusAgent.py
@@ -6,68 +6,122 @@
 #while avoiding pits and the wumpus, and then escape alive!
 
 #parameters for type of game
-gametype=0
-numarrows=0
-numwumpi=0
+from typing import List, Any
+gametype: int = 0
+numarrows: int = 0
+numwumpi: int = 0
 
-#determines state of ai
-foundgold = False
+#map
+map = []
+virtualoriginy = 0
+virtualoriginx = 0
+playerpositionx = 0
+playerpositiony = 0
+
+#determines searching or escaping state of AI
+foundgold: bool = False
 
 #actions AI can do
-moveup = 'N'
-movedown = 'S'
-moveleft = 'W'
-moveright = 'E'
+moveup: str = 'N'
+movedown: str = 'S'
+moveleft: str = 'W'
+moveright: str = 'E'
 
-shootup = 'SN'
-shootdown = 'SS'
-shootleft = 'SW'
-shootright = 'SE'
+shootup: str = 'SN'
+shootdown: str = 'SS'
+shootleft: str = 'SW'
+shootright: str = 'SE'
 
-climbout = 'C'
-grabgold = 'G'
+climbout: str = 'C'
+grabgold: str = 'G'
 
-#AI can have 2 states, states of AI are of the following
+#AI can either be searching or escaping
 # 1. Searching
 # 2. Escaping
-state = ''
+state: str = ''
 
-pastmoves = []
+#if a wumpus or pit is nearby, this will be flagged for the agent
+dangernear: bool = False
+
+#list of past moves, for memory
+pastmoves: list[str] = []
 
 def setParams(type, arrows, wumpi):
-    gametype = type
+    global gametype
+    global numarrows
+    global numwumpi
+    gametype= type
     numarrows = arrows
     numwumpi = wumpi
 
+    #sets map with nothing but start square
+    temp = ['START']
+    map.append(temp)
 
+
+movecount = 0
 def getMove(percept = ''):
 
-    nextmove = parsePercept(percept)
+    printMap()
 
-    if(percept == ''):
+    nextmove: str = parsePercept(percept)
+    editMapAndPlayerPosition(nextmove)
+
+    if percept == '':
         print("empty")
     else:
         print(percept)
 
     return nextmove
 
+def printMap():
+    for i in range(0, len(map)):
+        print("[", *map[i], "]")
+
+def editMapAndPlayerPosition(themove):
+    global playerpositionx
+    global playerpositiony
+    global virtualoriginy
+    global virtualoriginx
+
+    print("position: ", playerpositionx, playerpositiony)
+    print("virtual origin: ", virtualoriginx, virtualoriginy)
+
+    #tracks player position, also when discovering a new row or column,
+    # adds new column to list, updates virtual origin for AI
+    if themove == moveup:
+        if virtualoriginy + playerpositiony == 0:
+            map.insert(0, [0] * len(map[0]))
+            virtualoriginy = virtualoriginy + 1
+        playerpositiony = playerpositiony - 1
+
+    if themove == movedown:
+        if virtualoriginy - playerpositionx == 0:
+            map.append([0] * len(map[0]))
+        playerpositiony = playerpositiony + 1
+
+
+
 def parsePercept(percept):
     global foundgold
+    global movecount
     if(foundgold == False):
         state = 'Searching'
     elif(foundgold == True):
         state = 'Escaping'
 
-    move = moveup
-
-    #print(*pastmoves)
-
+    if movecount == 3:
+        move = moveup
+    else:
+        move = movedown
+    movecount = movecount + 1
     if(percept.__contains__('G')):
         foundgold = True
         move = grabgold
 
     if (percept.__contains__("U")):
         pastmoves.pop()
+
 
     pastmoves.append(move)
     return move
