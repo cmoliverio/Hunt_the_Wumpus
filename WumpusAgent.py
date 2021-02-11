a7baca0b8e1cdf504ed3d2002ae4b35d80243f4c
Gardian20 <gardian299@gmail.com>
2021-02-10 21:07:42 -0500

    Actual garbage.  Sorry.

--- WumpusAgent.py
+++ WumpusAgent.py
@@ -4,7 +4,7 @@
 # Assignment:Project 1: Hunt the Wumpus
 # Description: Agent designed to retrieve the gold from the cave
 # while avoiding pits and the wumpus, and then escape alive!
-
+import math
 # parameters for type of game
 gametype: int = 0
 numarrows: int = 0
@@ -49,7 +49,13 @@ state: str = ''
 dangernear: bool = False
 
 # list of past moves, for memory
-pastmoves: list[str] = []
+pastmoves: list = []
+
+#Remember where breezes were found
+breezeLocations = []
+breezePhase = 0
+pitsconfirmed = 0
+
 
 def setParams(type, arrows, wumpi):
     global gametype
@@ -85,7 +91,8 @@ def getMove(percept = ''):
     printMap()
     print()
     nextmove = parsePercept(percept)
-    editMapAndPlayerPosition(nextmove)
+    editMapAndPlayerPosition()
+
     pastmoves.append(nextmove)
 
     return nextmove
@@ -95,15 +102,128 @@ def checkBump(perceptstring):
         lastmove = pastmoves.pop()
         undoMoveSetLimitRemoveRowOrColumn(lastmove)
 
-def parsePercept(percept):
-    global foundgold
+def determineBreeze():
+    breezeLocationX = playerpositionx
+    breezeLocationY = playerpositiony
 
+def invertMove(move):
+    if move == moveleft:
+        return moveright
+    if move == moveright:
+        return moveleft
+    if move == moveup:
+        return movedown
+    if move == movedown:
+        return moveup
+
+def isValidMove(move):
+    newx = playerpositionx
+    newy = playerpositiony
+    if move == moveup:
+        newy -= 1
+    if move == movedown:
+        newy += 1
+    if move == moveleft:
+        newx -= 1
+    if move == moveright:
+        newx += 1
+
+    if newx not in range(minxpos,maxxpos):
+        return False
+    if newy not in range(minypos,maxypos):
+        return False
+    if newx + virtualoriginx in range(0,len(map)) and newy + virtualoriginy in range(0,len(map[0])):
+        token = map[newx + virtualoriginx][newy + virtualoriginy]
+        if token in ["S",0,"E"]:
+            return True
+        return False
+
+def parsePercept(percept):
+    print(percept)
+    global foundgold
+    global breezePhase
+    global pitsconfirmed
     #if(foundgold == False):
     #    state = 'Searching'
     #elif(foundgold == True):
     #    state = 'Escaping'
 
     move = moveleft
+
+
+
+    if len(breezeLocations) > 0:
+        if breezePhase == 0:
+            move = invertMove(pastmoves.pop())
+            breezePhase += 1
+        elif breezePhase == 1:
+            if playerpositionx == breezeLocations[0][0]:
+                if(isValidMove(moveup)):
+                    move = moveup
+            else:
+                if(isValidMove(moveleft)):
+                    move = moveleft
+            breezePhase += 1
+        elif breezePhase == 2 or breezePhase == 4:
+            if not playerpositionx == breezeLocations[0][0] and not playerpositiony == breezeLocations[0][1]:
+                move = invertMove(pastmoves.pop())
+            if "B" in percept:
+                pitsconfirmed += 1
+                if move == moveleft or move == moveright:
+                    map[virtualoriginx + playerpositionx][virtualoriginy + playerpositiony + math.copysign(1,breezeLocations[0][1] - playerpositiony)] = "H"
+
+                else:
+                    map[virtualoriginx + playerpositionx + math.copysign(1, breezeLocations[0][1] - playerpositionx)][virtualoriginy + playerpositiony] = "H"
+            breezePhase += 1
+        elif breezePhase == 3:
+            if playerpositionx == breezeLocations[0][0]:
+                if(isValidMove(movedown)):
+                    move = movedown
+            else:
+                if(isValidMove(moveright)):
+                    move = moveright
+            breezePhase += 1
+        elif breezePhase == 5:
+            confirmx = breezeLocations[0][0]
+            confirmy = breezeLocations[0][1]
+            if breezeLocations[0][0] < playerpositionx:
+                move = moveleft
+                confirmx -= 1
+            elif breezeLocations[0][0] > playerpositionx:
+                move = moveright
+                confirmx += 1
+            elif breezeLocations[0][1] < playerpositiony:
+                move = moveup
+                confirmy -= 1
+            else:
+                move = movedown
+                confirmy += 1
+            if(pitsconfirmed == 0):
+                map[virtualoriginx + confirmx][virtualoriginy + confirmy] = "H"
+
+            breezePhase = 0
+            pitsconfirmed = 0
+            del(breezeLocations[0])
+    else:
+        if (maxxpos > 500):
+            move = moveright
+        elif (maxypos > 500):
+            move = movedown
+        elif (minxpos < -500):
+            move = moveleft
+        elif (minypos < -500):
+            move = moveup
+
+
+
+    map[virtualoriginx + playerpositionx][virtualoriginy + playerpositiony] = "S" #indicate visited squares as "Safe"
+
+
+    if("B" in percept) and len(breezeLocations) == 0:
+        breezeLocations.append([playerpositionx,playerpositiony])
+
+    if(foundgold):
+        move = invertMove(pastmoves.pop())
 
     if (percept.__contains__('G')):
         move = grabgold
